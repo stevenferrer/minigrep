a2dc81d486eb25b973fa7d89024e3e408b9cbdd5
Steven Ferrer <steven.r.ferrer@gmail.com>
2022-10-12 20:52:32 +0800

    Output errors to stderr

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let conf = Config::build(&args).unwrap_or_else(|err| {
-        println!("error parsing arguments: {err}");
+        eprintln!("error parsing arguments: {err}");
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(conf) {
-        println!("application error: {e}");
+        eprintln!("application error: {e}");
         process::exit(1);
     }
 }
